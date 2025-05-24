020f9b356648d3cadf090a95e00afc78902d9638
Geapen <theogilat@gmail.com>
2025-05-24 15:37:08 +0200

    Fixing css element orders...

--- js/main.js
+++ js/main.js
@@ -9,12 +9,12 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 2, 5);
 
-const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+const canvas = document.getElementById('threeCanvas');
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true, antialias: true });
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x000000, 0);
 renderer.outputEncoding = THREE.sRGBEncoding;
-renderer.domElement.classList.add('three-canvas');
-document.body.appendChild(renderer.domElement);
 
 const light = new THREE.DirectionalLight(0xffffff, 3);
 light.position.set(5, 10, 7.5);
@@ -242,12 +242,15 @@ initModels().then(() => {
 window.addEventListener('resize', onWindowResize, false);
 
 function onWindowResize() {
-    const aspect = window.innerWidth / window.innerHeight;
+    const width = window.innerWidth;
+    const height = window.innerWidth * (9 / 16);
+    const aspect = width / height;
+
     camera.aspect = aspect;
     camera.updateProjectionMatrix();
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(width, height);
 
-    //if (aspect < 1) camera.position.set(0, 2.08, 5.1); // Phone recoil
-    if (aspect > 1) camera.position.set(0, 2, 5); // Regular screen
+    if (width < 768) camera.position.set(0, 2.08, 5.1);
+    else camera.position.set(0, 2, 5);
 }
